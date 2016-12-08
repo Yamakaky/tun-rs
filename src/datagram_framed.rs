cac6ea06e1e43f36d0117e385dc5e89e28e0b005
Yamakaky <yamakaky@yamaworld.fr>
2016-12-08 13:56:14 -0500

    Implement UdpCodec.

--- src/datagram_framed.rs
+++ src/datagram_framed.rs
@@ -1,6 +1,8 @@
 use std::io;
+use std::net::SocketAddr;
 
 use tokio_core::io::{Codec, EasyBuf};
+use tokio_core::net::UdpCodec;
 
 pub struct Parser;
 
@@ -15,5 +17,20 @@ impl Codec for Parser {
     fn encode(&mut self, msg: Vec<u8>, buf: &mut Vec<u8>) -> io::Result<()> {
         buf.extend_from_slice(msg.as_slice());
         Ok(())
+    }
+}
+
+impl UdpCodec for Parser {
+    type In = (SocketAddr, Vec<u8>);
+    type Out = (SocketAddr, Vec<u8>);
+
+    fn decode(&mut self, src: &SocketAddr, buf: &[u8]) -> io::Result<Self::In> {
+        Ok((src.clone(), buf.into()))
+    }
+
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> SocketAddr {
+        let (addr, data) = msg;
+        buf.extend(data);
+        addr
     }
 }
